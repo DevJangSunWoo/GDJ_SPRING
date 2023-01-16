68a044f4eada572e2a8bc77ef43c17a16bce5c7e
DevJangSunWoo <sw6yyrdn@gmail.com>
2023-01-16 10:02:57 +0900

    security-context.xml에  db와 연동하여 provider 제공하기 ,  SecrurtyService 클래스 생성

---
+++ HelloSpring/src/main/java/com/bs/spring/security/SecurityService.java
@@ -0,0 +1,43 @@
+package com.bs.spring.security;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.bs.spring.member.dao.MemberDao;
+import com.bs.spring.member.vo.Member;
+
+@Service
+public class SecurityService implements UserDetailsService {
+
+	
+	private MemberDao dao;
+	private SqlSessionTemplate session;
+	
+	
+	@Autowired
+	public SecurityService( MemberDao dao,SqlSessionTemplate session) {
+		this.dao=dao;
+		this.session=session;
+		
+	}
+	
+	
+	
+	
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		// username에 id가 들어감
+		Member m=dao.selectMemberById (session,Member.builder().userId(username).build());
+		
+		
+		
+		return m;
+	}
+
+}
