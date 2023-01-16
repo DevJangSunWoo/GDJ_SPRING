87f03d86a2158804f60e205f2b803f7405117007
DevJangSunWoo <sw6yyrdn@gmail.com>
2023-01-16 12:40:54 +0900

    1월 16일4교시/JpaMember 클래스를 생성-> 필드맴버 선언 -no identifier 에러발생->해결 @id 생성

---
+++ HelloSpring/src/main/java/com/bs/spring/jpa/model/entity/JpaMember.java
@@ -0,0 +1,27 @@
+package com.bs.spring.jpa.model.entity;
+
+import java.util.Date; //자바 꺼지 떄문에 util 로 임포트하기
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+//생성한 클래스를 jpa와 연동하는 Entity로 등록하려면 어노테이션을 이용한다.
+//@Enttity어노테이션 -> jpa 관리한느 db와 연동되는 객체를 의마함.
+
+
+//자바 코드로 되있는 테이블이라고 생각하기
+// jpa 관리한다는 것은  db랑 연결되는 클래스를 관리한다는 것
+@Data
+@Entity
+public class JpaMember {
+
+	@Id
+	private long memberNo;
+	
+	private String memberId;
+	private String memberPwd;
+	private int age;
+	private double height;
+	private Date enrollDate;
+}
